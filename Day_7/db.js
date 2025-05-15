9e3d42004798fdf1f3177001d158b9ee8540aca5
shruu1403 <shruti.gupta01412@gmail.com>
2025-05-15 19:53:26 +0530

    added env file

--- Day_7/db.js
+++ Day_7/db.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose")
-
-const connection=mongoose.connect("mongodb://127.0.0.1:27017/Day_7")
-
-
+const dotenv=require("dotenv")
+dotenv.config()
+const connection=mongoose.connect(process.env.mongoURL)
 module.exports={connection}
