f882e82c0aca1ff6ef4daec5b12dd1a5533f12f9
shruu1403 <shruti.gupta01412@gmail.com>
2025-07-04 00:27:43 +0530

    changes done

--- FULL_stack_CRUD_1/middlewares/auth.middleware.js
+++ FULL_stack_CRUD_1/middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const auth = async (req, res, next) => {
-    const token = req.headers.authorization;
+    const token = req.headers.authorization.split(" ")[1]
 
     try {
         if(token){
